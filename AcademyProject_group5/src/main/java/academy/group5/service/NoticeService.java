47ff0d13016d4131506ebbdae5b7558373b0644f
1-718-8 <1-718-8@15-717-8-PC>
2016-10-04 11:00:30 +0900

    I make noticeService interface, and I need feedback

---
+++ AcademyProject_group5/src/main/java/academy/group5/service/NoticeService.java
@@ -0,0 +1,29 @@
+package academy.group5.service;
+
+import java.util.List;
+
+import academy.group5.dto.Lecturenotice;
+
+public interface NoticeService {
+	/**
+	 * 전체 강의의 공지 가져오기 page=5
+	 * @param userId
+	 * @param page
+	 * @return
+	 */
+	List<Lecturenotice> AllLecturenoticeList(String userId, int page);
+	/**
+	 * 특정 강의의 전체 공지 가져오기 page=5
+	 * @param lectureId
+	 * @param lectureClass
+	 * @return
+	 */
+	List<Lecturenotice> LecturenoticeList(Integer lectureId, Integer lectureClass, int page);
+	/**
+	 * 강의의 상세 정보
+	 * @param lectureId
+	 * @return
+	 */
+	Lecturenotice LectureInfo(Integer lectureId);
+	
+}
