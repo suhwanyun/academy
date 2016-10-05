a22d2469bc719c8f5a1e89c5a521c2ed699b5b49
1-718-8 <1-718-8@15-717-8-PC>
2016-10-05 09:54:28 +0900

    lectureService->함수이름 변경 ManagerService->매니저 로그인, 상품 등록 수정 삭제, 강의 등록 수정 삭제 추가 MileageService->마일리지 전체목록,구매목록,사용 등 서비스 추가

--- AcademyProject_group5/src/main/java/academy/group5/service/ManagerService.java
+++ AcademyProject_group5/src/main/java/academy/group5/service/ManagerService.java
@@ -1,6 +1,9 @@
 package academy.group5.service;
 
+import academy.group5.dto.Lecture;
+import academy.group5.dto.Lecturetime;
 import academy.group5.dto.Manager;
+import academy.group5.dto.Mileageproduct;
 
 public interface ManagerService {
 	/**
@@ -10,4 +13,60 @@ public interface ManagerService {
 	 * @return
 	 */
 	Manager managerLogin(String managerId, String managerPass);
+	/**
+	 * 강의등록
+	 * @param lecture
+	 * @return
+	 */
+	boolean registerLecture(Lecture lecture);
+	/**
+	 * 강의 시간 등록
+	 * @param lecturetime
+	 * @return
+	 */
+	boolean registerLecturetime(Lecturetime lecturetime);
+	/**
+	 * 강의 내용 수정
+	 * @param lecture
+	 * @return
+	 */
+	boolean updateLecture(Lecture lecture);
+	/**
+	 * 강의 시간 내용 수정
+	 * @param lecturetime
+	 * @return
+	 */
+	boolean updateLecturetime(Lecturetime lecturetime);
+	/**
+	 * 강의삭제(강의에 시간까지 같이 삭제)
+	 * @param lectureId
+	 * @return
+	 */
+	boolean deleteLecture(Integer lectureId);
+	/**
+	 * 강의 시간삭제
+	 * @param lectureId
+	 * @param lectureStart
+	 * @return
+	 */
+	boolean deleteLecturetime(Integer lectureId, Integer lectureStart);
+	/**
+	 * 상품 등록
+	 * @param mileageProduct
+	 * @return
+	 */
+	boolean registerProduct(Mileageproduct mileageProduct);
+	/**
+	 * 상품 수정
+	 * @param mileageProduct
+	 * @return
+	 */
+	boolean updateProduct(Mileageproduct mileageProduct);
+	/**
+	 * 상품 삭제
+	 * @param productId
+	 * @return
+	 */
+	boolean deleteProduct(Integer productId);
+	
 }
