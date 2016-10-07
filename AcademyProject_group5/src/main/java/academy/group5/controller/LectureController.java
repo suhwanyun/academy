0bac3bb7d5e8243613d830912f2ac56efb3efc80
YSH <YSH@210.125.213.66>
2016-10-07 09:54:03 +0900

    controller update

--- AcademyProject_group5/src/main/java/academy/group5/controller/LectureController.java
+++ AcademyProject_group5/src/main/java/academy/group5/controller/LectureController.java
@@ -15,4 +15,53 @@ public class LectureController {
 		return "campus/lecture/lecture_list";
 	}
 	
+	// 학생이 선택한 강의의 메인 페이지
+	@RequestMapping(value="/lectureMain", method=RequestMethod.GET)
+	public String lectureMainPage(Model model){
+		
+		return "campus/lecture/lecture_main";
+	}
+	
+	// 학생이 선택한 강의의 게시판 페이지
+	@RequestMapping(value="/lectureBoard", method=RequestMethod.GET)
+	public String lectureBoard(Model model){
+		
+		return "campus/lecture/lecture_board";
+	}
+	
+	// 학생이 선택한 강의의 게시판 글 내용 확인
+	@RequestMapping(value="/lectureBoardInfo", method=RequestMethod.GET)
+	public String lectureBoardInfo(Model model){
+		
+		return "campus/lecture/lecture_board_info";
+	}
+	
+	// 학생이 선택한 강의의 게시판에 글 작성
+	@RequestMapping(value="/lectureBoardAdd", method=RequestMethod.GET)
+	public String lectureBoardAdd(Model model){
+		
+		return "campus/lecture/lecture_board_add";
+	}
+	
+	
+	// 학생이 선택한 강의의 공지사항 목록
+	@RequestMapping(value="/lectureNotiList", method=RequestMethod.GET)
+	public String lectureNotiList(Model model){
+		
+		return "campus/lecture/lecture_noti_list";
+	}
+	
+	// 학생이 선택한 강의의 공지사항 중 선택한 공지의 정보
+	@RequestMapping(value="/lectureBoardInfo", method=RequestMethod.GET)
+	public String lectureBoardInfo(Model model){
+		
+		return "campus/lecture/lecture_board_info";
+	}
+	
+	// 학생이 선택한 강의의 게시판에 글 작성
+	@RequestMapping(value="/lectureBoardAdd", method=RequestMethod.GET)
+	public String lectureBoardAdd(Model model){
+		
+		return "campus/lecture/lecture_board_add";
+	}
 }
