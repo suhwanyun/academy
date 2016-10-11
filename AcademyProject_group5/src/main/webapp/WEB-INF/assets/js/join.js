652640ddadd216b8b415cf4530c50370897a1d42
1-718-8 <1-718-8@15-717-8-PC>
2016-10-11 09:51:26 +0900

    .

--- AcademyProject_group5/src/main/webapp/WEB-INF/assets/js/join.js
+++ AcademyProject_group5/src/main/webapp/WEB-INF/assets/js/join.js
@@ -1,62 +1,54 @@
-var isChanged = true;
+var dupOk = false;
+var passSame = false;
 	function idCheck(x){
 		var ID_PATTERN = /^[a-z][a-z0-9_$@#]{3,11}$/i;
 		if(ID_PATTERN.test(x)){
-			$("#userId").attr('class', 'true');
 			return true;
 		}else{
-			$("#userId").attr('class', 'false');
 			return false;
 		}
-			
 	}
 	function passCheck(x){
 		var PASS_PATTERN = /^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{6,20}$/;
 		if(PASS_PATTERN.test(x)){
-			$("#userPass").attr('class', 'true');
 			return true;
-		}else
-			$("#userPass").attr('class', 'false');
+		}else{
 			return false;
+		}
+			
 	}
 	function nameCheck(x){
 		var NAME_PATTERN = /^[가-힣]{2,5}$/;
 		if(NAME_PATTERN.test(x)){
-			$("#userName").attr('class', 'true');
 			return true;
-		}else
-			$("#userName").attr('class', 'false');
+		}else{
 			return false;
+		}
 	}
 	function phoneCheck(x){
 		var PHONE_PATTERN = /^[0-9]{10,11}$/;
 		if(PHONE_PATTERN.test(x)){
-			$("#phoneNum").attr('class', 'true');
 			return true;
-		}else
-			$("#phoneNum").attr('class', 'false');
+		}else{
 			return false;
+		}
 	}
 	function questionCheck(x){
 		if(x.length>0&&x.length<=100){
-			$("#passQuestion").attr('class', 'true');
 			return true;
-		}else
-			$("#passQuestion").attr('class', 'false');
+		}else{
 			return false;
+		}
 	} 
 	function anserCheck(x){
 		if(x.length>0&&x.length<=20){
-			$("#passAnswer").attr('class', 'true');
 			return true;
-		}else
-			$("#passAnswer").attr('class', 'false');
+		}else{
 			return false;
+		}
 	}
 		$("#joinBtn").click(function(event){
-			
-			
-				if (isChanged&&idCheck($("#userId").val()) && passCheck($("#userPass").val()) 
+				if (passSame&&dupOk&&idCheck($("#userId").val()) && passCheck($("#userPass").val()) 
 						&& nameCheck($("#userName").val()) && phoneCheck($("#phoneNum").val())
 						&& questionCheck($("#passQuestion").val()) && anserCheck($("#passAnswer").val()) == true) {
 					alert("가입 성공");
@@ -68,7 +60,7 @@ var isChanged = true;
 			});
 		
 	$("#userId").change(function(){
-		isChanged = false;
+		dupOk = false;
 		$("#trueorfalse").attr('class', 'button alt falseButton');
 		if(idCheck($("#userId").val())){
 			$("#userId").attr('class', 'true');
@@ -77,62 +69,55 @@ var isChanged = true;
 		}
 	});
 	$("#userPass").change(function(){
-		isChanged = false;
 		if(passCheck($("#userPass").val())){
+			$("#userPass").attr('class', 'true');
 			if($("#passCheck").val()==$("#userPass").val()){
-				$("#userPass").attr('class', 'true');
-				isChanged = true;
+				$("#passCheck").attr('class', 'true');
+				passSame = true;
 				}else{
 					$("#passCheck").attr('class', 'false');
+					passSame = false;
 				}
 		}else{
 			$("#userPass").attr('class', 'false');
 		}	
 	});
 	$("#passCheck").change(function(){
-		isChanged = false;
 		if(passCheck($("#userPass").val())&&$("#passCheck").val()==$("#userPass").val()){
 			$("#passCheck").attr('class', 'true');
-			isChanged = true;
+			passSame = true;
 		}else{
 			$("#passCheck").attr('class', 'false');
+			passSame =false;
 		}
 			
 	});
 	$("#userName").change(function(){
-		isChanged = false;
 		if(nameCheck($("#userName").val())){
 			$("#userName").attr('class', 'true');
-			isChanged = true;
 		}else{
 			$("#userName").attr('class', 'false');	
 		}
 			
 	});
 	$("#phoneNum").change(function(){
-		isChanged = false;
 		if(phoneCheck($("#phoneNum").val())){
 			$("#phoneNum").attr('class', 'true');
-			isChanged = true;
 		}else{
 			$("#phoneNum").attr('class', 'false');
 		}
 			
 	});
 	$("#passQuestion").change(function(){
-		isChanged = false;
 		if(questionCheck($("#passQuestion").val())){
 			$("#passQuestion").attr('class', 'true');
-			isChanged = true;
 		}else{
 			$("#passQuestion").attr('class', 'false');
 		}
 	});
 	$("#passAnswer").change(function(){
-		isChanged = false;
 		if(anserCheck($("#passAnswer").val())){
 			$("#passAnswer").attr('class', 'true');
-			isChanged = true;
 		}else{
 			$("#passAnswer").attr('class', 'false');
 		}
@@ -152,11 +137,12 @@ var isChanged = true;
 
 							if (res == "true") {
 								alert("사용가능한 ID 입니다.");
-								isChanged = true;
+								dupOk = true;
 								$("#trueorfalse").attr('class', 'button alt trueButton');
 								
 							} else {
 								alert("이미 있는 ID 입니다.");
+								dupOk = false;
 								$("#trueorfalse").attr('class', 'button alt falseButton');
 							}
 
