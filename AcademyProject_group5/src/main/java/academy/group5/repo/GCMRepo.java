9622504438b37141be6282a638d970110481c2b8
YSH <YSH@210.125.213.66>
2016-10-13 19:50:48 +0900

    GCM 송신 Repository 구현

--- AcademyProject_group5/src/main/java/academy/group5/repo/GCMRepo.java
+++ AcademyProject_group5/src/main/java/academy/group5/repo/GCMRepo.java
@@ -6,8 +6,7 @@ import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import academy.group5.dto.NotificationSetting;
-import academy.group5.dto.Notifications;
+import academy.group5.dto.Lecture;
 
 @Repository
 public class GCMRepo {
@@ -27,5 +26,17 @@ public class GCMRepo {
 	public String getPresident(int lectureId) {
 		String stmt = GCM_NS + "president";
 		return session.selectOne(stmt, lectureId);
+	}
+	
+	/** 해당 강의를 듣는 모든 학생 */
+	public List<String> getLectureApplyUser(Lecture data) {
+		String stmt = GCM_NS + "lectureApplyUser";
+		return session.selectList(stmt, data);
+	}
+	
+	/** 특정 학생 */
+	public String getOneUser(String userId) {
+		String stmt = GCM_NS + "oneUser";
+		return session.selectOne(stmt, userId);
 	}
 }
