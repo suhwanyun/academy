22874d40d2e2ac6b1aa22553e8aaee388b6bb95b
YSH <YSH@210.125.213.66>
2016-10-23 17:49:29 +0900

    폰 로그인 처리

--- AcademyProject_group5/src/main/java/academy/group5/controller/PhoneController.java
+++ AcademyProject_group5/src/main/java/academy/group5/controller/PhoneController.java
@@ -1,13 +1,18 @@
 package academy.group5.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import academy.group5.dto.UserData;
 import academy.group5.repo.GCMRepo;
+import academy.group5.service.LoginService;
 import academy.group5.service.PhoneService;
 import academy.group5.util.GCM;
 
@@ -15,10 +20,28 @@ import academy.group5.util.GCM;
 public class PhoneController {
 	
 	@Autowired
+	LoginService loginService;
+	
+	@Autowired
 	PhoneService phoneService;
 	
 	@Autowired
 	GCMRepo gcmRepo;
+	
+	/** 어플 로그인 */
+	@RequestMapping(value="/appLogin", method=RequestMethod.POST)
+	public String login(Model model, HttpSession session,
+			@RequestParam String userId, @RequestParam String userPass){
+		
+		UserData data = loginService.login(userId, userPass);
+				
+		if(data != null){	
+			session.setAttribute("user", data);	
+		} else {
+			model.addAttribute("msg", "로그인에 실패하였습니다.");
+		}
+		return "index";
+	}
 	
 	/** 식사(먹거리)추천 게시판에 글 작성 */
 	@RequestMapping(value="/registGCM", method=RequestMethod.POST)
