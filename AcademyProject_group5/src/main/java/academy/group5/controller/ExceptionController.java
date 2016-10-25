a6ce9d235bd00e855a6c3dd726f1e80f0a91c0a2
YSH <YSH@210.125.213.66>
2016-10-25 11:25:48 +0900

    .

--- AcademyProject_group5/src/main/java/academy/group5/controller/ExceptionController.java
+++ AcademyProject_group5/src/main/java/academy/group5/controller/ExceptionController.java
@@ -1,12 +1,10 @@
 package academy.group5.controller;
 
-import org.apache.ibatis.exceptions.PersistenceException;
-import org.springframework.dao.DuplicateKeyException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
-
-import academy.group5.exception.WrongRequestException;
 
 /**
  * 예외처리 컨트롤러
@@ -16,9 +14,11 @@ import academy.group5.exception.WrongRequestException;
 @Controller
 public class ExceptionController {
 	
-	@ExceptionHandler({	PersistenceException.class,
-						DuplicateKeyException.class})
+	private static final Logger logger = LoggerFactory.getLogger(ExceptionController.class);
+	
+	@ExceptionHandler(DataAccessException.class)
 	public String dbException(Exception e){	
+		logger.trace("\n\nDataAccessException 정보 :\n{}\n", e);
 		return "/error/error_page";
 	}
 
