04844e3d76e62e37d6cc4c0bfccb941b983b10c5
YSH <YSH@210.125.213.66>
2016-11-01 14:48:45 +0900

    에러 발생시 메인으로 이동하도록 수정

--- AcademyProject_group5/src/main/java/academy/group5/controller/IndexController.java
+++ AcademyProject_group5/src/main/java/academy/group5/controller/IndexController.java
@@ -151,6 +151,8 @@ public class IndexController {
 		if(lectureList.size() != 0) {
 			model.addAttribute("lectureList", lectureList);
 		}
+		// 이후 페이지에서 에러 발생시 이동할 페이지를 현재 페이지로 설정
+		session.setAttribute("errorGotoPage", "/lectureManage/main");
 		return "/manage/lecture/lecture";
 	}
 	
